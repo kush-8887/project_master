507e11c04e8886addc0bd65590f6041802d480d3
ArushDua <156054349+ArushDua@users.noreply.github.com>
2024-07-24 20:14:28 +0530

    sample complete

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,39 +1,52 @@
 import React from 'react'
+import picture from './resources/image.png'
 
 const Login = () => {
   return (
-    <div className="grid">
-     <nav className="flex justify-between h-[80px] w-[100%] border-[2px] border-black mt-0">
-     <div className="left text-5xl mt-2">
+    <div className='bg-blue-950'>
+     <nav className="flex justify-between h-[80px] w-[100%]  mt-0 bg-blue-900">
+     <div className="left text-5xl mt-2 text-white">
                   Salesalizer
             </div>
 
             <div className="right flex flex-col">
-                  <ul className='flex mx-5 my-5 p-2'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Services</li>
+                  <ul className='flex mx-5 my-5 text-white p-2'>
+                    <li className='p-1'>Home</li>
+                    <li  className='p-1'>About</li>
+                    <li  className='p-1'>Services</li>
                   </ul>
             </div>
      </nav>
-     <div className="img border-2 border-black  w-[50%]">
-      
-     </div>
-    <div className=' w-[500px] h-[90vh] flex  flex-col justify-center items-center border-black border-2 ml-[66%]'>
+    <div className="bottom flex justify-between w-[100%] h-[100%]">
+
+    <div className="left w-[120vh] h-[90vh] flex  flex-col  items-center ]">
+            <img src={picture} className=' w-[100vh]'/>
+    </div>
+
+
+    <div className='right w-[70vh] h-[90vh] flex  flex-col  items-center  bg-white'>
+      <div className="form mt-[30vh] flex  flex-col  items-center">
+
       <div className="head">
-        <h1 className='text-lg font-bold'>Login</h1>
+        <h1 className='text-lg  font-bold'>Login</h1>
       </div>
+
       <div className="input p-1 justify-center items-center flex font-bold">
        Email
         <input type="text" className="rounded-xl border-2 border-slate-950"/>
       </div>
-      <div className="input p-1   font-bold ">
+
+      <div className="input p-1   font-bold  ml-[-40px] ">
         Password
         <input type="text" className="rounded-xl border-2 border-slate-950 "/>
       </div>
+
       <div className="input ">
         <button className='bg-black text-white p-2 font-bold'>Submit</button>
       </div>
+
+      </div>
+    </div>
     </div>
     </div>
   )
